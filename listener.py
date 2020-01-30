bcee1dda269f2bdeb80aca9dd08bf4b66631ba77
Shaikh Rashid <sk.f.rashid@gmail.com>
2020-01-30 06:33:26 -0500

    refactor listener.py to run workflow
    For now, the `listener.py` will be handling executing the bcftools
    process until CWL is completed.

--- listener.py
+++ listener.py
@@ -1,46 +1,85 @@
-import json
+#!/usr/bin/env python
+"""
+doc
+"""
+# import json
+import os
+import subprocess
 
 from flask import Flask, jsonify, request
-# from minio import Minio
-from wes_client import util
-from wes_client.util import modify_jsonyaml_paths
 
-# Access the Minio server if needed, access keys may vary based on minio config
-# client = Minio('127.0.0.1:9000', access_key="minioadmin", secret_key="minioadmin", secure=False)
+from minio import Minio
+from minio.error import ResponseError
 
-app = Flask(__name__)
-@app.route('/events', methods=['POST'])
+# from wes_client import util
+# from wes_client.util import modify_jsonyaml_paths
+
+
+APP = Flask(__name__)
+@APP.route('/events', methods=['POST'])
 def main():
-    print(request.data)
-    # Minio events result in two calls, so we ignore the first
+    """
+    doc
+    """
+    # minio events result in two calls, so we ignore the first
     if not request.data:
         return "First Request Ignored", 406
+
+    client = Minio('minio:9000',
+                   access_key=str(os.environ["MINIO_ACCESS_KEY"]),
+                   secret_key=str(os.environ["MINIO_SECRET_KEY"]),
+                   secure=False)
+
+    # Get a full object
+    try:
+        client.fget_object(
+            'samples', 'unprocessed/NA18537.vcf.gz', 'NA18537.vcf.gz')
+    except ResponseError as err:
+        print(err)
+
+    # Run bcftools via subprocess and output to local directory
+    subprocess.run(['bcftools', 'norm', '-d', 'all', 'NA18537.vcf.gz',
+                    '-o', 'NA18537-normalized.vcf'],
+                   check=True, stdout=subprocess.PIPE, universal_newlines=True)
+
+    # Put 'NA18537-normalized.vcf' into minio/samples/processed/
+    try:
+        client.fput_object('samples',
+                           'processed/NA18537-normalized.vcf',
+                           'NA18537-normalized.vcf')
+    except ResponseError as err:
+        print(err)
+
+    return "Workflow successful"
 
     # Get the name of the added file
     # newMinioFile = json.loads(request.data)['Key']
 
     # Retrieve Workflow Data from workflowInput.json
-    inFile = open("workflowInput.json", "r")
-    if inFile.mode == "r":
-        param = inFile.read()
-    param = json.loads(param)
+    # inFile = open("workflowInput.json", "r")
+    # if inFile.mode == "r":
+        # param = inFile.read()
+    # param = json.loads(param)
 
     # Fix key naming for request
-    param["workflow_params"] = param.pop("workflow_params/json_path")
-    param["workflow_url"] = param.pop("workflow_url/name")
+    # param["workflow_params"] = param.pop("workflow_params/json_path")
+    # param["workflow_url"] = param.pop("workflow_url/name")
     # Split the comma seperated attachments into a list for input
-    param["workflow_attachment"] = param["workflow_attachment"].split(',')
+    # param["workflow_attachment"] = param["workflow_attachment"].split(',')
 
-    # This is the line that client uses to contruct a json object representing the parameters
+    # construct a json object representing the parameters
     # But this gives drive specific paths
-    param["workflow_params"] = modify_jsonyaml_paths(param["workflow_params"])
+    # param["workflow_params"] = modify_jsonyaml_paths(param["workflow_params"])
 
-    clientObject = util.WESClient({'auth': '', 'proto': 'http', 'host': "wes-server:5000"})
-    req = clientObject.run(param["workflow_url"], param["workflow_params"], param["workflow_attachment"])
+    # clientObject = util.WESClient(
+    # {'auth': '', 'proto': 'http', 'host': "wes-server:5000"})
+    # req = clientObject.run(
+    # param["workflow_url"], param["workflow_params"], param["workflow_attachment"])
 
-    return "Workflow Request Sent"
+    # return "Workflow Request Sent"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    APP.run(host='0.0.0.0', port=8080)
 # TODO: ue python to run bcftools norm in process
 # TODO: Generalize input workflow further
