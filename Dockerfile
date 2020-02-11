9440362288943a709a14615093bd68b45a836a5c
Samarth Patel <samarth patel@rt5182ml-ccm.sickkids.ca>
2020-02-11 15:09:03 -0500

    Reorganized workflow, completed listener and fixed listener Dockerfile bugs

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM python:3.7.6
 RUN pip install flask
 RUN pip install requests
 RUN pip install minio
+RUN pip install werkzeug==0.16.1
+RUN pip install connexion==1.4.2
 RUN pip install wes-service
 RUN apt-get update && apt-get install -y \
 	samtools bcftools vim nano
 COPY listener.py /srv/listener.py
+COPY tests/workflowInput.json /srv/tests/workflowInput.json
+COPY workflow-data/ /srv/workflow-data/
 WORKDIR /srv
 ENTRYPOINT [ "python", "/srv/listener.py"]
--- listener.py
+++ listener.py
@@ -8,11 +8,11 @@ import subprocess
 
 from flask import Flask, jsonify, request
 
-from minio import Minio
-from minio.error import ResponseError
+# from minio import Minio
+# from minio.error import ResponseError
 
-# from wes_client import util
-# from wes_client.util import modify_jsonyaml_paths
+from wes_client import util
+from wes_client.util import modify_jsonyaml_paths
 
 
 APP = Flask(__name__)
@@ -25,62 +25,58 @@ def main():
     if not request.data:
         return "First Request Ignored", 406
 
-    # Get the name of the added file
-    dataobject = json.loads(request.data)['Key']
-    path = dataobject.split("/")
+    # # Get the name of the added file
+    # dataobject = json.loads(request.data)['Key']
+    # path = dataobject.split("/")
 
-    client = Minio('minio:9000',
-                   access_key=str(os.environ["MINIO_ACCESS_KEY"]),
-                   secret_key=str(os.environ["MINIO_SECRET_KEY"]),
-                   secure=False)
+    # client = Minio('minio:9000',
+    #                access_key=str(os.environ["MINIO_ACCESS_KEY"]),
+    #                secret_key=str(os.environ["MINIO_SECRET_KEY"]),
+    #                secure=False)
 
-    # Get a full object
-    try:
-        client.fget_object(
-            str(path[0]), str(path[1] + "/" + path[2]), str(path[2]))
-    except ResponseError as err:
-        print(err)
+    # # Get a full object
+    # try:
+    #     client.fget_object(
+    #         str(path[0]), str(path[1] + "/" + path[2]), str(path[2]))
+    # except ResponseError as err:
+    #     print(err)
 
-    # Run bcftools via subprocess and output to local directory
-    outfile = str(path[2]).split(".")
-    outfile = str(outfile[0] + "-normalized.vcf")
+    # # Run bcftools via subprocess and output to local directory
+    # outfile = str(path[2]).split(".")
+    # outfile = str(outfile[0] + "-normalized.vcf")
 
-    subprocess.run(['bcftools', 'norm', '-d', 'all', str(path[2]),
-                    '-o', str(outfile)],check=True,
-                   stdout=subprocess.PIPE, universal_newlines=True)
+    # subprocess.run(['bcftools', 'norm', '-d', 'all', str(path[2]),
+    #                 '-o', str(outfile)],check=True,
+    #                stdout=subprocess.PIPE, universal_newlines=True)
 
-    # Put 'NA18537-normalized.vcf' into minio/samples/processed/
-    try:
-        client.fput_object(str(path[0]), str("processed/" + outfile),
-                           str(outfile))
-    except ResponseError as err:
-        print(err)
+    # # Put 'NA18537-normalized.vcf' into minio/samples/processed/
+    # try:
+    #     client.fput_object(str(path[0]), str("processed/" + outfile),
+    #                        str(outfile))
+    # except ResponseError as err:
+    #     print(err)
 
-    return "Workflow successful"
-
+    # return "Workflow successful"
 
     # Retrieve Workflow Data from workflowInput.json
-    # inFile = open("workflowInput.json", "r")
-    # if inFile.mode == "r":
-        # param = inFile.read()
-    # param = json.loads(param)
+    inFile = open("tests/workflowInput.json", "r")
+    if inFile.mode == "r":
+        param = inFile.read()
+    param = json.loads(param)
 
-    # Fix key naming for request
-    # param["workflow_params"] = param.pop("workflow_params/json_path")
-    # param["workflow_url"] = param.pop("workflow_url/name")
     # Split the comma seperated attachments into a list for input
-    # param["workflow_attachment"] = param["workflow_attachment"].split(',')
+    param["workflow_attachment"] = param["workflow_attachment"].split(',')
 
     # construct a json object representing the parameters
     # But this gives drive specific paths
-    # param["workflow_params"] = modify_jsonyaml_paths(param["workflow_params"])
+    param["workflow_params"] = modify_jsonyaml_paths(param["workflow_params"])
 
-    # clientObject = util.WESClient(
-    # {'auth': '', 'proto': 'http', 'host': "wes-server:5000"})
-    # req = clientObject.run(
-    # param["workflow_url"], param["workflow_params"], param["workflow_attachment"])
+    clientObject = util.WESClient(
+        {'auth': '', 'proto': 'http', 'host': "wes-server:5000"})
+    req = clientObject.run(
+        param["workflow_url"], param["workflow_params"], param["workflow_attachment"])
 
-    # return "Workflow Request Sent"
+    return "Workflow Request Sent"
 
 
 if __name__ == '__main__':
