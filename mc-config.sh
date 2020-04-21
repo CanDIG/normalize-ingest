d8fe932324184035cdf9842103387ec427dee2c4
samarthvp <samarth28@hotmail.com>
2020-04-21 10:49:34 -0400

    Updated mc-config.sh with newer env vars

--- mc-config.sh
+++ mc-config.sh
@@ -6,11 +6,11 @@ source "$(pwd)"/.env
 shopt -s expand_aliases
 alias mc="docker exec -it $(basename "`pwd`")_mc_1 mc"
 
-mc config host add minio http://minio:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
+mc config host add minio http://minio:$MINIO_UI_PORT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
 
-mc mb minio/samples/processed/
+mc mb minio/$MINIO_PROCESSED_DIR
 mc mb minio/samples/unprocessed/
-mc mb minio/drs/
+mc mb minio/$MINIO_DRS_BUCKET
 
 mc admin config set minio notify_webhook:1 queue_limit="0" endpoint="http://listener:${LISTENER_PORT}/events"
 mc admin service restart minio
